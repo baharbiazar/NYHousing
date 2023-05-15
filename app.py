95c59374a05c5e5fcd94d555fd0c111d57002c1d
baharbiazar <baharbiazar@gmail.com>
2023-05-14 17:40:50 -0700

    updates

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.sidebar.header('Specify Input Parameters')
 
 def user_input_features():
     bedrooms = st.sidebar.slider('bedrooms',int(0), int(20), int(3))
-    bathrooms = st.sidebar.slider('bathrooms', int(0), int(20), int(2))
+    bathrooms = st.sidebar.slider('bathrooms', int(1), int(10), int(2))
     yearBuilt = st.sidebar.slider('yearBuilt', int(1800), int(2023), int(1980))
     property_tax = st.sidebar.slider('propTax', float(0.5), float(1), float(0.8))
     living_area = st.sidebar.slider('livingArea', int(100), int(8000), int(2500))
