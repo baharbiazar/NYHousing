967e7056118509ec35d944e0e3c456cb543b92b6
baharbiazar <baharbiazar@gmail.com>
2023-05-14 17:54:20 -0700

    updates

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ st_shap(shap.force_plot(explainer.expected_value, shap_values, user_df.columns.t
 
 shap.summary_plot(shap_values, user_df, plot_type="bar")
 st.subheader('Summary plot of SHAP values')
-shap.summary_plot(shap_values, user_df)
+#shap.summary_plot(shap_values, user_df)
 
 # Create the summary plot object
 
